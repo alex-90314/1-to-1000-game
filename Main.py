4e14130cdb97ab48c69765317b10d6118f0d915a
Alexander Sando <boricua422012+github@gmail.com>
2023-08-25 18:28:04 -0400

    finish setting up main game start

--- Main.py
+++ Main.py
@@ -1,6 +1,17 @@
-print('''Welcome to the show. The rules of the game are as follows:
-      1.
-      2.
-      3.
-      Let's Go!''')
-cls
+import os, time
+
+#clear the console and show the instructions
+os.system("cls")
+print('''Welcome to the show.
+The instructions are as follows:
+The game has 20 slots which you will populate using numbers that are randomly generated and given to you from 1-1000.
+If you get a number higher than the previous numbers and there are no slots for it to go into, you will lose :)
+''')
+time.sleep(1)
+skip_start = input() #allows the player to skip the instructions
+print ("Let's Go! (Press Enter to continue)")
+next_game = input() #holds the player until they press enter so they have time to read the rules
+
+#clear the screen to start the game
+os.system("cls")
+print("hi")
